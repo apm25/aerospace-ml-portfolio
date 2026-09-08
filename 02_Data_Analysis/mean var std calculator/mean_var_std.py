a5d7ed2b4662d8b3c04244465ae6ee32b0d14221
Agastya Mehta <agastyamehta7@gmail.com>
2026-09-08 19:57:58 +0100

    Add comments

--- 02_Data_Analysis/mean var std calculator/mean_var_std.py
+++ 02_Data_Analysis/mean var std calculator/mean_var_std.py
@@ -1,17 +1,29 @@
 import numpy as np
 
 def calculate(inputlist):
+
+    #Raise ValueError if input list length is < 9
     if len(inputlist) != 9:
         raise ValueError("List must contain nine numbers.")
+    
+    #Organise input list into NumPy array
     new_array = np.array([inputlist[0:3], inputlist[3:6], inputlist[6:9]], dtype=np.float64)
+
+    #Initialise output dictionary
     output_dict = {'mean': [],'variance': [], 'standard deviation': [], 'max': [], 'min': [], 'sum': []}
+
+    #Initialise for loop for 3 cases
     for i in range(3):
+
+        #Initialise lists of each operation
         meanlist = []
         varlist = []
         sdlist = []
         maxlist = []
         minlist = []
         sumlist = []
+
+        #Rows logic
         if i == 0:
             meanlist.append([float(np.mean(new_array[0])),float(np.mean(new_array[1])),float(np.mean(new_array[2]))])
             varlist.append([float(np.var(new_array[0])),float(np.var(new_array[1])),float(np.var(new_array[2]))])
@@ -19,6 +31,8 @@ def calculate(inputlist):
             maxlist.append([int(np.max(new_array[0])),int(np.max(new_array[1])),int(np.max(new_array[2]))])
             minlist.append([int(np.min(new_array[0])),int(np.min(new_array[1])),int(np.min(new_array[2]))])
             sumlist.append([int(np.sum(new_array[0])),int(np.sum(new_array[1])),int(np.sum(new_array[2]))])
+
+        #Columns logic
         if i== 1:
             meanlist.append([float(np.mean(new_array[:,0])),float(np.mean(new_array[:,1])),float(np.mean(new_array[:,2]))])
             varlist.append([float(np.var(new_array[:,0])),float(np.var(new_array[:,1])),float(np.var(new_array[:,2]))])
@@ -26,6 +40,8 @@ def calculate(inputlist):
             maxlist.append([int(np.max(new_array[:,0])),int(np.max(new_array[:,1])),int(np.max(new_array[:,2]))])
             minlist.append([int(np.min(new_array[:,0])),int(np.min(new_array[:,1])),int(np.min(new_array[:,2]))])
             sumlist.append([int(np.sum(new_array[:,0])),int(np.sum(new_array[:,1])),int(np.sum(new_array[:,2]))])
+
+        #Flattened array logic
         if i==2:
             meanlist = float(np.mean(new_array.flatten()))
             varlist = float(np.var(new_array.flatten()))
@@ -33,6 +49,8 @@ def calculate(inputlist):
             maxlist = int(np.max(new_array.flatten()))
             minlist = int(np.min(new_array.flatten()))
             sumlist = int(np.sum(new_array.flatten()))
+
+        #Append complete item lists to output dictionary
         output_dict['mean'].append(meanlist)
         output_dict['variance'].append(varlist)
         output_dict['standard deviation'].append(sdlist)
@@ -43,4 +61,3 @@ def calculate(inputlist):
                 
   # return calculations
     return output_dict
-print(calculate([0,1,2,3,4,5,6,7,8]))

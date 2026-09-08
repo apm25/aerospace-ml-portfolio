f8db8c40ef0660f8a6d86aaa5144a38168cfef96
Agastya Mehta <agastyamehta7@gmail.com>
2026-09-08 19:03:47 +0100

    Remove duplicate files from root directory

--- 02_Data_Analysis/mean var std calculator/mean_var_std.py
+++ 02_Data_Analysis/mean var std calculator/mean_var_std.py
@@ -4,5 +4,6 @@ def calculate(inputlist):
     if len(inputlist) != 9:
         raise ValueError("List must contain nine numbers.")
     new_array = np.array([inputlist[0:3], inputlist[3:6], inputlist[6:9]], dtype=np.float64)
-    new_array = np.array([])
-  # return calculationsan
+    output_dict = {'mean': [],'variance': [], 'standard deviation': [], 'max': [], 'min': [], 'sum': []}
+    output_dict['mean'] = [[float(np.mean())]]
+  # return calculations

4bc8ff8ab3eb08272bcbc6bb020edbf48a44ded3
Agastya Mehta <agastyamehta7@gmail.com>
2026-09-21 18:50:12 +0100

    Initialise sandbox test file and finish line plot logic

--- 02_Data_Analysis/Time_Series_Visualiser/time_series_visualizer.py
+++ 02_Data_Analysis/Time_Series_Visualiser/time_series_visualizer.py
@@ -5,17 +5,21 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv("fcc-forum-pageviews.csv").set_index('date')
 
 # Clean data
-df = None
+df = df[((df['value'] > df['value'].quantile(0.025)) & (df['value'] < df['value'].quantile(0.975)))]
 
 
 def draw_line_plot():
     # Draw line plot
 
-
-
+    x_data = df['date']
+    y_data = df['value']
+    fig = plt.plot(x_data,y_data,'r')
+    plt.ylabel('Page Views')
+    plt.xlabel('Date')
+    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
 
 
     # Save image and return fig (don't change this part)
